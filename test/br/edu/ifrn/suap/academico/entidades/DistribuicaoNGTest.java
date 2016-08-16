9302a977a6d7abcc3948429cea5fffcb3e2ebceb
JoabMendes <joab.mendes.r2d2@gmail.com>
2016-08-15 23:12:52 -0300

    Added pom.xml and test cases (No tests functions written yet)

---
+++ test/br/edu/ifrn/suap/academico/entidades/DistribuicaoNGTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.suap.academico.entidades;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+
+/**
+ *
+ * @author joab
+ */
+@Test
+public class DistribuicaoNGTest {
+	
+	
+	public void testPeriodoValido() {
+		//assertThat(Project.builder().title(TITLE1).build()).isEqualTo(Project.builder().title(TITLE1).build());
+	}
+	
+	public void testPeriodoFechado(){
+	
+	}
+	
+}
